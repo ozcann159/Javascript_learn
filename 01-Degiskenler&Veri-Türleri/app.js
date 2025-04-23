7c48011614bbd9d677f49fdb62ae195715117dc1
Elif Özcan <ozcann159@gmail.com>
2025-04-23 10:57:29 +0300

    degisken&veritürleri-test

---
+++ 01-Degiskenler&Veri-Türleri/app.js
@@ -0,0 +1,1 @@
+//!Degişkenler
---
+++ app.js
@@ -0,0 +1,65 @@
+//Document write ile cıktı verme
+// document.writeln("hello World");
+// document.writeln("Elif Özcan");
+// document.writeln(55);
+// document.writeln(true);
+// document.writeln(null);
+
+//console.log metodu kullanımı
+
+// console.log("Hello World");
+// console.log("Enes Bayram");
+// console.log(true);
+// console.log({ name: "Elif", surname: "Özcan" });
+
+// let a = 15;
+// let b = 5;
+// console.log("İki sayinin toplami:" + (a + b));
+
+// alert("Dikkat");
+
+// var - let - const
+
+// function selamVer() {
+//   var selam = "Herkese selam bu bir fonksiyondur";
+//   console.log(selam);
+// }
+
+// selamVer();
+
+let isim = "Elif";
+console.log(isim);
+
+let sayi1 = 10;
+let sayi2 = 5;
+console.log(sayi1);
+console.log(typeof sayi1);
+console.log(sayi1 + sayi2);
+
+let x = null;
+console.log(x);
+
+let t;
+
+let insan = {};
+
+let rakamlar = [1, 2, 3, 4];
+
+let func = function () {
+  console.log("merhaba");
+};
+
+let sayi9 = "5";
+let sayi8 = 10;
+
+console.log("Notlarınızın toplamı:" + (sayi9 + sayi8));
+
+let not1 = 100;
+let not2 = 50;
+
+sonuc = (not1 + not2) / 2;
+console.log(sonuc);
+
+let a = 10;
+a++;
+console.log(a);
