6f36a096ce01159127016cf6802d5f86ac5cfbd2
Elif Özcan <ozcann159@gmail.com>
2025-04-25 14:23:02 +0300

    Day1 completed

---
+++ Day1/introduction.js
@@ -0,0 +1,18 @@
+//console.log("Bu yazı introduction.js'den geldi")
+
+console.log(typeof "merhaba");
+console.log(typeof 5);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+// get first name
+
+let getFirstName
+let name = 'Elif'
+let age = 28
+let isSoftwareActive = true
+let money = undefined
+
+console.log(name, age,isSoftwareActive,money) 
---
+++ Prompt/app.js
@@ -0,0 +1,6 @@
+//! Promptlar
+
+//* Promptlar kullanıcıdan girdi almak için kullanılır. Bu fonksiyon bir pencere açar ve kullanıcıdan bir metin girmesini isteyerek girilen metni geri döndürür.
+
+let Name = prompt("Adınızı Girin");
+console.log("Merhaba," + Name + "!")
